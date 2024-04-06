35e31c14fe8d2d2e72c5d85cead3b5bdc6d7b50c
Arjun Krishna <arjunkrishna@gmail.com>
2024-04-05 22:01:16 -0400

    refactored trip/trip_crew.py

--- features/trip/plan_trip_page.py
+++ features/trip/plan_trip_page.py
@@ -5,6 +5,7 @@ import streamlit as st
 import datetime
 import schema
 from features.trip.trip_details import TripDetails
+from features.trip.trip_crew import TripCrew
 from home_page import HomePage
 
 
@@ -65,55 +66,3 @@ class PlanTripPage:
         self.display_form()
         self.process_data()
 
-class TripCrew:
-
-    def __init__(self, origin, cities, date_range, interests):
-        self.cities = cities
-        self.origin = origin
-        self.interests = interests
-        self.date_range = date_range
-        self.output_placeholder = st.empty()
-
-    def run(self):
-        agents = TripAgents()
-        tasks = TripTasks()
-
-        city_selector_agent = agents.city_selection_agent()
-        local_expert_agent = agents.local_expert()
-        travel_concierge_agent = agents.travel_concierge()
-
-        identify_task = tasks.identify_task(
-            city_selector_agent,
-            self.origin,
-            self.cities,
-            self.interests,
-            self.date_range
-        )
-
-        gather_task = tasks.gather_task(
-            local_expert_agent,
-            self.origin,
-            self.interests,
-            self.date_range
-        )
-
-        plan_task = tasks.plan_task(
-            travel_concierge_agent,
-            self.origin,
-            self.interests,
-            self.date_range
-        )
-
-        crew = Crew(
-            agents=[
-                city_selector_agent, local_expert_agent, travel_concierge_agent
-            ],
-            tasks=[identify_task, gather_task, plan_task],
-            verbose=True
-        )
-
-        result = crew.kickoff()
-        return result
-
-    def display_result(self, result):
-        self.output_placeholder.markdown(result)        
---
+++ features/trip/trip_crew.py
@@ -0,0 +1,61 @@
+from crewai import Crew
+from features.trip.trip_agents import TripAgents
+from features.trip.trip_tasks import TripTasks
+import streamlit as st
+import datetime
+import schema
+
+
+
+class TripCrew:
+
+    def __init__(self, origin, cities, date_range, interests):
+        self.cities = cities
+        self.origin = origin
+        self.interests = interests
+        self.date_range = date_range
+        self.output_placeholder = st.empty()
+
+    def run(self):
+        agents = TripAgents()
+        tasks = TripTasks()
+
+        city_selector_agent = agents.city_selection_agent()
+        local_expert_agent = agents.local_expert()
+        travel_concierge_agent = agents.travel_concierge()
+
+        identify_task = tasks.identify_task(
+            city_selector_agent,
+            self.origin,
+            self.cities,
+            self.interests,
+            self.date_range
+        )
+
+        gather_task = tasks.gather_task(
+            local_expert_agent,
+            self.origin,
+            self.interests,
+            self.date_range
+        )
+
+        plan_task = tasks.plan_task(
+            travel_concierge_agent,
+            self.origin,
+            self.interests,
+            self.date_range
+        )
+
+        crew = Crew(
+            agents=[
+                city_selector_agent, local_expert_agent, travel_concierge_agent
+            ],
+            tasks=[identify_task, gather_task, plan_task],
+            verbose=True
+        )
+
+        result = crew.kickoff()
+        return result
+
+    def display_result(self, result):
+        self.output_placeholder.markdown(result)        
